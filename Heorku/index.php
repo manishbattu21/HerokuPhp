00e3947a46cd91b0f7a1e4fef952f1f0a3d5a2e9
manishbattu21 <46189600+manishbattu21@users.noreply.github.com>
2019-07-17 15:23:10 +0530

    Update index.php

--- Heorku/index.php
+++ Heorku/index.php
@@ -1,7 +1,6 @@
 <html>
 <body>
 <?php
-
 define("USERNAME", "manishbattu@dev.com");
 define("PASSWORD", "manomani@2169");
 define("SECURITY_TOKEN", "oqwAHteZvrpluSMW2LsRiDps");
@@ -12,7 +11,7 @@ $mySforceConnection->createConnection("PartnerWSDL.xml");
 $mySforceConnection->login(USERNAME, PASSWORD.SECURITY_TOKEN);
 if($mySforceConnection !=NULL)
 {
-	echo '<b>Connected successfully to <b>'.USERNAME.'<br><br>';
+	echo 'Connected successfully to '.USERNAME.'<br><br>';
 }
 else
 {
@@ -23,12 +22,31 @@ $response = $mySforceConnection->query($query);
 
 foreach ($response->records as $record)
 {
-$id =$record->Id;
-echo '<tr>
-<td>'.$record->Id.'</td>&nbsp;&nbsp;&nbsp;
-<td>'.$record->fields->FirstName.'</td>&nbsp;&nbsp;&nbsp;
-<td>'.$record->fields->LastName.'</td>&nbsp;&nbsp;&nbsp;
-</tr><br>';
+	$id =$record->Id;
+	echo '<tr>
+	<td>'.$record->Id.'</td>&nbsp;&nbsp;&nbsp;
+	<td>'.$record->fields->FirstName.'</td>&nbsp;&nbsp;&nbsp;
+	<td>'.$record->fields->LastName.'</td>&nbsp;&nbsp;&nbsp;
+	<td>'.$record->fields->Phone.'</td>&nbsp;&nbsp;&nbsp;
+	</tr><br>';
+
+$query1 = "SELECT ContentDocumentId FROM ContentDocumentLink WHERE LinkedEntityId ='" .$id ."'";
+$queryResult = $mySforceConnection->query($query1);
+if (!empty($queryResult->records)) {
+	foreach($queryResult->records as $contentDocumentId) {
+		$contdocId = $contentDocumentId->fields->ContentDocumentId;
+		
+$query2 = "SELECT Id,Title,VersionData FROM ContentVersion WHERE ContentDocumentId ='" .$contdocId ."'";
+$queryResult1 = $mySforceConnection->query($query2);
+
+	if (!empty($queryResult1->records)) { // if condition to check for the records
+		foreach($queryResult1->records as $attachment) {
+// to print the attachment name
+echo $attachment->fields->Title.'&nbsp;&nbsp;&nbsp;'.$attachment->Id.'</br></br>';
+		}
+	}
+	}
+}
 }
 ?>
 </body>
